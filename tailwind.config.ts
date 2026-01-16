9e37c51a805d706fce9fc0977471df2b20abe0f5
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2026-01-16 13:46:15 +0530

    added cyan

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
             colors: {
                 background: "var(--background)",
                 foreground: "var(--foreground)",
+                'brand-cyan': "var(--brand-cyan)",
             },
         },
     },
